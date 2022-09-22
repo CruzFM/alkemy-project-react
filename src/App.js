fc377acb091feb5dd8367c39151d12d99327e65d
Jakiro12 <eclipsisnocte@gmail.com>
2022-09-22 00:30:35 -0300

    formik 1 input basico

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import React from 'react';
+import { Formik,Form,Field} from 'formik'
 
 function App() {
-  return (
-    <div>
-      <h1>Hola lauta, perdón la tardanza jaja.</h1>
-      <h1>hola fernando</h1>
-      <h1>En esta rama realizo cambios sin fucionar</h1>
+ 
+//el HtmlFor sirve para linkear el label con el input, si hago click
+// en el label Frist name me lleva a escribir en el input a traves del id = '...', una relacion innecesaria
+//Field es el input en Formik
+  return (<div>
+    <Formik
+     initialValues={{
+      firstName: '',
+    }}
+    onSubmit={(values)=>{
+      console.log(values)
+    }}
+    >
+       <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="Jane" type='email' />       
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+   
     </div>
   );
 }
