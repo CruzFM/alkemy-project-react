1d3662b9890e34f2e4a24cc29d3d7039c7fa2837
Fernando Cruz <cruzfernandomartin94@gmail.com>
2022-09-21 23:23:45 -0300

    basic form imported to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { SignupForm } from './components/SignupForm';
 
 function App() {
   return (
     <div>
       <h1>Hola lauta, perdón la tardanza jaja.</h1>
       <h1>hola fernando</h1>
+      <SignupForm />
     </div>
   );
 }
